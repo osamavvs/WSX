7aed5ed4c7e0c38000ed4b4d8a61ceefd399e980
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-28 23:08:39 +0300

    Update client.py

--- core/client.py
+++ core/client.py
@@ -1,19 +1,14 @@
 from pyrogram import Client
-from pytgcalls import Client as PyTgCalls
+from pytgcalls import PyTgCalls
 import config
-import asyncio
 
-# تهيئة تطبيق التليجرام مع حل مشكلة تزامن الوقت والجلسة
+# تهيئة تطبيق التليجرام
 app = Client(
     "CristalBot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    sleep_threshold=180,  # ليتجنب حظر تليجرام المؤقت
-    in_memory=True        # أهم خيار لحل خطأ [16] msg_id is too low
+    bot_token=config.BOT_TOKEN
 )
 
-# تشغيل المحرك لنسخة 2026 (v3)
+# تهيئة محرك المكالمات الصوتية
 call_py = PyTgCalls(app)
-
-print("✅ Cristal Music Engine (v3) is Synchronizing...")
