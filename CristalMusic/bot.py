8737cb92002e3df498b730c94876a6b604acad3d
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-28 19:29:50 +0300

    Update bot.py

--- CristalMusic/bot.py
+++ CristalMusic/bot.py
@@ -1,1 +1,17 @@
+import asyncio
+from pyrogram import filters, idle
+from core.client import app, call_py
+from config import OWNER_ID
 
+@app.on_message(filters.command("start"))
+async def start(c, m):
+    await m.reply_text("✨ أهلاً بك في سورس كرستال بايثون\nالبوت يعمل الآن بنظام المجلدات القديم ✅")
+
+async def main():
+    await app.start()
+    await call_py.start()
+    print("💎 Cristal Music is Online!")
+    await idle()
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
