cbe28904a8f83d00ea930387557e665a01fc0f3c
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-28 23:25:46 +0300

    Update client.py

--- core/client.py
+++ core/client.py
@@ -1,24 +1,14 @@
 from pyrogram import Client
-import pytgcalls
+from pytgcalls import PyTgCalls
 import config
-import time
 
-# تهيئة تطبيق التليجرام مع ميزة 'إزاحة الوقت'
+# تهيئة تطبيق التليجرام
 app = Client(
     "CristalBot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    sleep_threshold=240,
-    in_memory=True  # ضروري جداً عشان ما يستخدم جلسات قديمة
+    bot_token=config.BOT_TOKEN
 )
 
-# محاولة الاستدعاء حسب النسخة الموجودة في هيروكو
-try:
-    from pytgcalls import Client as PyTgCalls
-    call_py = PyTgCalls(app)
-except ImportError:
-    from pytgcalls import PyTgCalls
-    call_py = PyTgCalls(app)
-
-print("✅ Cristal Engine: Time Synchronizing...")
+# تهيئة محرك المكالمات الصوتية
+call_py = PyTgCalls(app)
