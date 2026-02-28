6df451e01ece2550a63ee57a64a7cf08fc5513ff
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-28 06:15:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,22 @@ from pyrogram import Client, idle
 from pytgcalls import PyTgCalls
 from config import API_ID, API_HASH, STRING_SESSION
 
-# إعداد العميل
 app = Client(
-    "MusicUserbot",
+    "MusicBot",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=STRING_SESSION,
-    plugins=dict(root="plugins")
+    plugins={"root": "plugins"}
 )
 
-# محرك المكالمات
 call_py = PyTgCalls(app)
 
-async def start_bot():
-    print("--------------------------")
+async def start():
     await app.start()
     await call_py.start()
-    print("✅ البوت اشتغل والمكالمة جاهزة!")
-    print("--------------------------")
+    print("✅ تم تشغيل البوت بنجاح!")
     await idle()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_bot())
+    loop.run_until_complete(start())
