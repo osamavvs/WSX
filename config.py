83fc27fa96444830047e68685c3c2f94441048e4
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-28 06:30:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 from os import getenv
 
-# جلب البيانات من هيروكو
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-STRING_SESSION = getenv("STRING_SESSION") # تأكد أن الاسم هنا يطابق ما في هيروكو
-OWNER_ID = int(getenv("OWNER_ID"))
+STRING_SESSION = getenv("STRING_SESSION")
